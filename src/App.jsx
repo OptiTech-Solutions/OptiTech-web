c63e287af9669e599d6210f294319552b4ce25d0
eliamuzu <peamuzu003@st.ug.edu.gh>
2026-02-23 04:35:24 +0000

    feat: initialize React project with Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className='p-5 bg-black'>Hello</div>
+    </>
+  )
+}
+
+export default App
